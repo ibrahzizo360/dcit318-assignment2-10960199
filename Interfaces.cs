dff98e399b9f27e5215646c0670f829faa1fec74
ibrahzizo360 <ibrahimaziz200000@gmail.com>
2024-06-17 19:50:10 +0000

    Done with Interfaces demo

---
+++ Interfaces.cs
@@ -0,0 +1,32 @@
+﻿public interface IMovable
+{
+    void Move();
+}
+
+public class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Car is moving");
+    }
+}
+
+public class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving");
+    }
+}
+
+public class Interfaces
+{
+    public static void Main(string[] args)
+    {
+        Car car = new Car();
+        Bicycle bicycle = new Bicycle();
+
+        car.Move();
+        bicycle.Move();
+    }
+}
